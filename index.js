7870f323ebd776d41a06f12a1946a4e1bd749e10
YUSUF <yusu@duck.com>
2023-02-08 14:38:50 +0300

    x

--- index.js
+++ index.js
@@ -5,6 +5,34 @@ const cors = require('cors');
 app.use(cors());
 app.options('*', cors());
 
+function date(timestamp) {
+    const x = timestamp.toString() + '000';
+
+    return new Date(Number(x)).toLocaleTimeString('en-US', {
+        year: 'numeric',
+        month: 'long',
+        day: '2-digit',
+        hour: '2-digit',
+        minute: '2-digit',
+        second: '2-digit',
+        hour12: true
+    });
+}
+
+function dateEn(timestamp) {
+    const x = timestamp.toString() + '000';
+
+    return new Date(Number(x)).toLocaleTimeString('en-US', {
+        year: 'numeric',
+        month: 'long',
+        day: '2-digit',
+        hour: '2-digit',
+        minute: '2-digit',
+        second: '2-digit',
+        hour12: true
+    });
+}
+
 function cleanLocation(text) {
     return text.replace(/undefined/g, '').trim();
 }
@@ -15,7 +43,7 @@ function dateToTimestamp(date, time) {
 
     const d = new Date(`${year}-${month}-${day}`);
 
-    return d.getTime() + ((hour - 3) * 60 * 60 * 1000) + (minute * 60 * 1000) + (second * 1000);
+    return (d.getTime() + ((hour - 3) * 60 * 60 * 1000) + (minute * 60 * 1000) + (second * 1000)) / 1000;
 }
 
 function clean(arr) {
@@ -37,20 +65,54 @@ app.get('/all', (req, res) => {
                     const earthquake = clean(earthquakes[i].split(/\s+/g).filter((item) => item !== '' && item !== '.'));
 
                     const obj = {
+                        id: i + 1,
                         timestamp: dateToTimestamp(earthquake[0], earthquake[1]),
                         date: earthquake[0],
                         time: earthquake[1],
-                        latitude: earthquake[2],
-                        longitude: earthquake[3],
-                        depth: earthquake[4],
-                        magnitude: earthquake[5],
+                        fullDate: `${earthquake[0]} ${earthquake[1]}`,
+                        longDate: date(dateToTimestamp(earthquake[0], earthquake[1])) + '^' + dateEn(dateToTimestamp(earthquake[0], earthquake[1])),
+                        latitude: Number(earthquake[2]),
+                        longitude: Number(earthquake[3]),
+                        coordinates: [
+                            Number(earthquake[2]),
+                            Number(earthquake[3])
+                        ],
+                        depth: Number(earthquake[4]),
+                        magnitude: Number(earthquake[5]),
                     }
 
-                    if (earthquake.length > 8) {
-                        obj['moment'] = earthquake[6];
-                        obj['location'] = cleanLocation(earthquake[7] + ' ' + earthquake[8]);
+                    if (earthquake.length >= 8 && !isNaN(earthquake[6])) {
+                        obj['moment'] = Number(earthquake[6]);
+
+                        const loc = cleanLocation(earthquake.slice(7, earthquake.length).join(' '));
+                        const regex = /([\s\S]*?)REVIZE01/;
+
+                        if (regex.test(loc)) {
+                            obj['location'] = cleanLocation(regex.exec(loc)[1].trim());
+                        } else {
+                            obj['location'] = cleanLocation(earthquake.slice(7, earthquake.length).join(' '));
+                        }
+
+                        if (loc.includes('(')) {
+                            obj['base'] = loc.split('(')[1].split(')')[0];
+                        } else {
+                            obj['base'] = loc;
+                        }
                     } else {
-                        obj['location'] = cleanLocation(earthquake[6] + ' ' + earthquake[7]);
+                        const loc = cleanLocation(earthquake.slice(6, earthquake.length).join(' '));
+                        const regex = /([\s\S]*?)REVIZE01/;
+
+                        if (regex.test(loc)) {
+                            obj['location'] = cleanLocation(regex.exec(loc)[1].trim());
+                        } else {
+                            obj['location'] = cleanLocation(earthquake.slice(6, earthquake.length).join(' '));
+                        }
+
+                        if (loc.includes('(')) {
+                            obj['base'] = loc.split('(')[1].split(')')[0];
+                        } else {
+                            obj['base'] = loc;
+                        }
                     }
 
                     arr.push(obj);
@@ -60,6 +122,11 @@ app.get('/all', (req, res) => {
                     return res.status(200).json({
                         status: 200,
                         result: arr.slice(0, req.query.limit)
+                    });
+                } else if (req.query.get && req.query.get > 0 && !isNaN(req.query.get)) {
+                    return res.status(200).json({
+                        status: 200,
+                        result: arr[req.query.get - 1]
                     });
                 }
 
@@ -87,20 +154,154 @@ app.get('/latest', (req, res) => {
                     timestamp: dateToTimestamp(earthquake[0], earthquake[1]),
                     date: earthquake[0],
                     time: earthquake[1],
-                    latitude: earthquake[2],
-                    longitude: earthquake[3],
-                    depth: earthquake[4],
-                    magnitude: earthquake[5],
+                    fullDate: `${earthquake[0]} ${earthquake[1]}`,
+                    longDate: date(dateToTimestamp(earthquake[0], earthquake[1])) + '^' + dateEn(dateToTimestamp(earthquake[0], earthquake[1])),
+                    latitude: Number(earthquake[2]),
+                    longitude: Number(earthquake[3]),
+                    coordinates: [
+                        Number(earthquake[2]),
+                        Number(earthquake[3])
+                    ],
+                    depth: Number(earthquake[4]),
+                    magnitude: Number(earthquake[5]),
                 }
 
-                if (earthquake.length > 8) {
-                    obj['moment'] = earthquake[6];
-                    obj['location'] = cleanLocation(earthquake[7] + ' ' + earthquake[8]);
+                if (earthquake.length >= 8 && !isNaN(earthquake[6])) {
+                    obj['moment'] = Number(earthquake[6]);
+
+                    const loc = cleanLocation(earthquake.slice(7, earthquake.length).join(' '));
+                    const regex = /([\s\S]*?)REVIZE01/;
+
+                    if (regex.test(loc)) {
+                        obj['location'] = cleanLocation(regex.exec(loc)[1].trim());
+                    } else {
+                        obj['location'] = cleanLocation(earthquake.slice(7, earthquake.length).join(' '));
+                    }
+
+                    if (loc.includes('(')) {
+                        obj['base'] = loc.split('(')[1].split(')')[0];
+                    } else {
+                        obj['base'] = loc;
+                    }
                 } else {
-                    obj['location'] = cleanLocation(earthquake[6] + ' ' + earthquake[7]);
+                    const loc = cleanLocation(earthquake.slice(6, earthquake.length).join(' '));
+                    const regex = /([\s\S]*?)REVIZE01/;
+
+                    if (regex.test(loc)) {
+                        obj['location'] = cleanLocation(regex.exec(loc)[1].trim());
+                    } else {
+                        obj['location'] = cleanLocation(earthquake.slice(6, earthquake.length).join(' '));
+                    }
+
+                    if (loc.includes('(')) {
+                        obj['base'] = loc.split('(')[1].split(')')[0];
+                    } else {
+                        obj['base'] = loc;
+                    }
                 }
 
                 arr.push(obj);
+
+                return res.status(200).json({
+                    status: 200,
+                    result: arr
+                });
+            }
+        });
+});
+
+app.get('/search', (req, res) => {
+    get('http://www.koeri.boun.edu.tr/scripts/lst7.asp')
+        .then((response) => {
+            const regex = /--------------([\s\S]*?)<\/pre>/;
+            const match = regex.exec(response.data);
+
+            if (match) {
+                const earthquakes = match[1].split('').slice(48, match[1].split('').length).join('').trim().split(/\n/g);
+                let cleanedEarthquakes = [];
+                let arr = [];
+
+                for (let i = 0; i < earthquakes.length; i++) {
+                    const earthquake = clean(earthquakes[i].split(/\s+/g).filter((item) => item !== '' && item !== '.'));
+
+                    const obj = {
+                        id: i + 1,
+                        timestamp: dateToTimestamp(earthquake[0], earthquake[1]),
+                        date: earthquake[0],
+                        time: earthquake[1],
+                        fullDate: `${earthquake[0]} ${earthquake[1]}`,
+                        longDate: date(dateToTimestamp(earthquake[0], earthquake[1])) + '^' + dateEn(dateToTimestamp(earthquake[0], earthquake[1])),
+                        latitude: Number(earthquake[2]),
+                        longitude: Number(earthquake[3]),
+                        coordinates: [
+                            Number(earthquake[2]),
+                            Number(earthquake[3])
+                        ],
+                        depth: Number(earthquake[4]),
+                        magnitude: Number(earthquake[5]),
+                    }
+
+                    if (earthquake.length >= 8 && !isNaN(earthquake[6])) {
+                        obj['moment'] = Number(earthquake[6]);
+
+                        const loc = cleanLocation(earthquake.slice(7, earthquake.length).join(' '));
+                        const regex = /([\s\S]*?)REVIZE01/;
+
+                        if (regex.test(loc)) {
+                            obj['location'] = cleanLocation(regex.exec(loc)[1].trim());
+                        } else {
+                            obj['location'] = cleanLocation(earthquake.slice(7, earthquake.length).join(' '));
+                        }
+
+                        if (loc.includes('(')) {
+                            obj['base'] = loc.split('(')[1].split(')')[0];
+                        } else {
+                            obj['base'] = loc;
+                        }
+                    } else {
+                        const loc = cleanLocation(earthquake.slice(6, earthquake.length).join(' '));
+                        const regex = /([\s\S]*?)REVIZE01/;
+
+                        if (regex.test(loc)) {
+                            obj['location'] = cleanLocation(regex.exec(loc)[1].trim());
+                        } else {
+                            obj['location'] = cleanLocation(earthquake.slice(6, earthquake.length).join(' '));
+                        }
+
+                        if (loc.includes('(')) {
+                            obj['base'] = loc.split('(')[1].split(')')[0];
+                        } else {
+                            obj['base'] = loc;
+                        }
+                    }
+
+                    cleanedEarthquakes.push(obj);
+                }
+
+                if (req.query.base && req.query.base !== '' && req.query.base.trim().length > 0) {
+                    arr.push(...cleanedEarthquakes.filter((item) => item.base.toLowerCase() === req.query.base.toLowerCase()));
+                } else if (req.query.magnitude && req.query.magnitude !== '' && req.query.magnitude.trim().length > 0 && !isNaN(req.query.magnitude)) {
+                    arr.push(...cleanedEarthquakes.filter((item) => item.magnitude === Number(req.query.magnitude)));
+                } else if (req.query.day && req.query.day !== '' && req.query.day.trim().length > 0 && !isNaN(req.query.day)) {
+                    const d = req.query.day.length > 1 ? req.query.day : '0' + req.query.day;
+
+                    arr.push(...cleanedEarthquakes.filter((item) => item.date.split('.')[2] === d));
+                } else if (req.query.month && req.query.month !== '' && req.query.month.trim().length > 0 && !isNaN(req.query.month)) {
+                    const m = req.query.month.length > 1 ? req.query.month : '0' + req.query.month;
+
+                    arr.push(...cleanedEarthquakes.filter((item) => item.date.split('.')[1] === m));
+                } else if (req.query.hour && req.query.hour !== '' && req.query.hour.trim().length > 0 && !isNaN(req.query.hour)) {
+                    const h = req.query.hour.length > 1 ? req.query.hour : '0' + req.query.hour;
+
+                    arr.push(...cleanedEarthquakes.filter((item) => item.time.split(':')[0] === h));
+                }
+
+                if (arr.length === 0) {
+                    return res.status(404).json({
+                        status: 404,
+                        message: 'No earthquakes found.'
+                    });
+                }
 
                 return res.status(200).json({
                     status: 200,
@@ -123,7 +324,19 @@ app.get('/', (req, res) => {
                 endpoint: '/all',
                 description: 'Returns the last 500 earthquakes.',
                 query: {
-                    limit: 'Returns the last n earthquakes. (n is a number)'
+                    limit: 'Returns the last n earthquakes. (n is a number)',
+                    get: 'Returns the nth earthquake. (n is a number)',
+                }
+            },
+            {
+                endpoint: '/search',
+                description: 'Searches earthquakes by day, month, hour, base and magnitude.',
+                query: {
+                    day: 'Returns earthquakes on the given day. (day is a number)',
+                    month: 'Returns earthquakes on the given month. (month is a number)',
+                    hour: 'Returns earthquakes on the given hour. (hour is a number)',
+                    base: 'Returns earthquakes on the given base. (base is a string)',
+                    magnitude: 'Returns earthquakes on the given magnitude. (magnitude is a number)',
                 }
             },
             {
